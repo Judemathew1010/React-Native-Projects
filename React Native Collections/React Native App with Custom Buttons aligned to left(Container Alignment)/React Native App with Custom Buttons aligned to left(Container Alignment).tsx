73e1af2864ca10b5815da25a0b0845efa090fdf7
Judemathew1010 <113626348+Judemathew1010@users.noreply.github.com>
2023-03-10 23:43:47 +0530

    Create React Native App with Custom Buttons aligned to left(Container Alignment).tsx

---
+++ React Native Collections/React Native App with Custom Buttons aligned to left(Container Alignment)/React Native App with Custom Buttons aligned to left(Container Alignment).tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Linking } from 'react-native';
+
+interface Item {
+  id: string;
+  title: string;
+}
+
+const DATA: Item[] = [
+  { id: '1', title: 'Item 1' },
+  { id: '2', title: 'Item 2' },
+  { id: '3', title: 'Item 3' },
+  { id: '4', title: 'Item 4' },
+  { id: '5', title: 'Item 5' },
+];
+
+export default function App() {
+  const [items, setItems] = useState<Item[]>(DATA);
+
+  const handleItemClick = (itemId: string) => {
+    let youtubeLink = '';
+    switch (itemId) {
+      case '1':
+        youtubeLink = 'https://www.youtube.com/watch?v=swXmY-7eTRo';
+        break;
+      case '2':
+        youtubeLink = 'https://www.youtube.com/watch?v=rKCL_MEXqJo';
+        break;
+      case '3':
+        youtubeLink = 'https://www.youtube.com/watch?v=NUl-a3GZznQ';
+        break;
+      case '4':
+        youtubeLink = 'https://www.youtube.com/watch?v=bsFXgfbj8Bc';
+        break;
+      case '5':
+        youtubeLink = 'https://www.youtube.com/watch?v=UNbyT7wPwk4';
+        break;
+      default:
+        break;
+    }
+    // Redirect to the appropriate YouTube link
+    Linking.openURL(youtubeLink);
+  };
+
+  const renderRow = (rowItems: Item[]) => (
+    <View style={styles.row}>
+      {rowItems.map((item) => (
+        <TouchableOpacity
+          key={item.id}
+          onPress={() => handleItemClick(item.id)}
+          style={styles.item}
+        >
+          <Text style={styles.title}>{item.title}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+
+  const rows: Item[][] = [
+    [DATA[0], DATA[1], DATA[2]], [DATA[3], DATA[4]],
+  ];
+
+  return (
+    <View style={styles.container}>
+      {rows.map((row, index) => (
+        <View key={index}>{renderRow(row)}</View>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e6f0ff',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  row: {
+    flexDirection: 'row',
+    marginStart:10,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  item: {
+    
+    width: 120,
+    height: 60,
+    marginEnd:6,
+    marginBottom:6,
+    borderRadius: 10,
+    backgroundColor: '#0066ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    color: '#ffffff',
+  },
+});
